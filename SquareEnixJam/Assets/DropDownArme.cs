bd2e8fa6f936eee417d6bb33041cc22ca3e0d173
Anthony Teixeira <anthony.teixeira.at@gmail.com>
2020-02-22 21:34:12 -0500

    Nouveau push avec les armes

---
+++ SquareEnixJam/Assets/DropDownArme.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DropDownArme : MonoBehaviour
+{
+    public Guerrier guerrier;
+
+    public TMP_Dropdown dropdown;
+
+    private void Update()
+    {
+        if(dropdown.value == 0)
+        {
+
+        }
+
+        if (dropdown.value == 1)
+        {
+
+        }
+
+        if (dropdown.value == 2)
+        {
+
+        }
+
+        if (dropdown.value == 3)
+        {
+
+        }
+
+        if (dropdown.value == 4)
+        {
+
+        }
+
+        if (dropdown.value == 0)
+        {
+
+        }
+    }
+
+}
--- SquareEnixJam/Assets/Monster/Rat.cs
+++ SquareEnixJam/Assets/Monster/Rat.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class Rat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
->>>>>>> master
-    }
 }

e78216c1afc6f8855647b51fc6c1c1ba1535a487
RENCUREL <pierre.rencurel@orange.fr>
2020-02-23 14:23:12 -0500

    Added texts
    DONE : Added all texts

---
+++ SquareEnixJam/Assets/ATKText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ATKText : MonoBehaviour
+{
+    public Guerrier guerrier;
+
+    public Text text1;
+    public string text;
+
+
+    private void Update()
+    {
+        text = guerrier.ATK.ToString();
+        text1.text = text;
+    }
+}
---
+++ SquareEnixJam/Assets/DEFText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DEFText : MonoBehaviour
+{
+    public Guerrier guerrier;
+
+    public Text text1;
+    public string text;
+
+
+    private void Update()
+    {
+        text = guerrier.DEF.ToString();
+        text1.text = text;
+    }
+}
